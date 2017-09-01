47e821f0b3ffd07caea43be60149ddbbc2692afb
Adam Hansen <adamlars90@gmail.com>
2017-09-01 17:11:47 +0200

    Started working on setting RestAPI up for customers

---
+++ CustomerRestAPI/Controllers/CustomersController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAppBLL;
+using RestAppBLL.BusinessObjects;
+
+namespace CustomerRestAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class CustomersController : Controller
+    {
+        private readonly BLLFacade _facade = new BLLFacade();
+
+        // GET: api/Customers
+        [HttpGet]
+        public IEnumerable<CustomerBO> Get()
+        {
+            return _facade.CustomerService.GetAll();
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}", Name = "Get")]
+        public CustomerBO Get(int id)
+        {
+            return _facade.CustomerService.GetById(id);
+        }
+        
+        // POST: api/Customers
+        [HttpPost]
+        public void Post([FromBody]CustomerBO customer)
+        {
+            _facade.CustomerService.Create(customer);
+        }
+        
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _facade.CustomerService.Delete(id);
+        }
+    }
+}
